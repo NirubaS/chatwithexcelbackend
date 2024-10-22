681dedb7dd070cef03746be73633b48fdc8e3887
NirubaS <niruba.s@goml.io>
2024-10-22 13:23:27 +0530

    Update router.py

--- router.py
+++ router.py
@@ -161,6 +161,4 @@ async def resolve_customer_handler(request: Request, db=Depends(get_db)):
     except Exception as e:
         return {"error": str(e)}
     
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+
